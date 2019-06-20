ecc6369c0b7e1dff689722f5387d1d6e3b90be13
Alex Alanya <alexandro.alanya@gmail.com>
2019-06-20 15:48:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,12 +6,19 @@ class App < Sinatra::Base
 		<h1>Hello World</h1>
 	end
 
-	get '/' do
-	  erb :index
-	end
 
-	get "/info" do
-	  "Testing the info page"
-	end
-	
+		get '/' do
+		  erb :index
+		end
+
+		get "/info" do
+		  erb :info
+		end
+
+
+		get "/info" do
+	  	"Testing the info page"
+		end
+
+
 end
